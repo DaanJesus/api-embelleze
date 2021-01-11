824118f5cee4e82bb296b9538f1ac6f623186f4e
DaanJesus <danilo25oliveira@gmail.com>
2021-01-11 18:16:06 -0300

    config to connect in mongoDB

--- node_sequelize/src/database/index.js
+++ node_sequelize/src/database/index.js
@@ -1,12 +1,34 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-try {
-    mongoose.connect('mongodb://localhost/admin', {useNewUrlParser: true});
-    mongoose.Promise = global.Promise;
 
-    console.log('Connection has been established successfully.');
-} catch {
-    console.error('Unable to connect to the database: ', error)
+const URL_MONGO = "mongodb+srv://forSale2021:forSale2021@forsaledatabase.ukynm.mongodb.net/test";
+
+const options = {
+    autoIndex: true,
+    reconectTries: Number.MAX_VALUE,
+    reconectInterval: 500,
+    poolSize: 10,
+    bufferMaxEntries: 0,
+    keepAlive: true,
+    useNewUrlParser: true,
+    user: process.env.LOGINDB,
+    pass: process.env.PASSWORDDB
 }
 
+mongoose.disconnect();
+mongoose.Promise = require('bluebird');
+
+const connectWithRetry = () => {
+    mongoose.connect(URL_MONGO, options).then(
+        () => {
+            console.log("Success connect to: " + URL_MONGO);
+        },
+        err => {
+            console.log("Error connect to: " + URL_MONGO + ' - ' + err);
+        }
+    );
+}
+
+connectWithRetry();
 module.exports = mongoose;
